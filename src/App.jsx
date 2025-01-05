d2640499013b91723e6c6229c602ae4f0d3eaa09
vishvamitra4 <vishvamitrakumarsingh@gmail.com>
2025-01-05 08:00:02 +0530

    Update: change baseURL

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css';
 import AppRoutes from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:401";
+// axios.defaults.baseURL = "http://localhost:401";
+axios.defaults.baseURL = "https://skillg-backend.onrender.com";
 axios.defaults.withCredentials = true;
 const App = () => {
   return (
