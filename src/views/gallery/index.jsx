1241018e7bf7c7714b0a5ade4643b39dc1283296
vishvamitra4 <vishvamitrakumarsingh@gmail.com>
2025-01-09 05:30:45 +0530

    Update: gallert section

--- src/views/gallery/index.jsx
+++ src/views/gallery/index.jsx
@@ -9,10 +9,10 @@ function Gallery() {
             <div class="grid grid-cols-4 md:grid-cols-4 gap-4">
                 <div class="grid gap-4">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/14025956/pexels-photo-14025956.jpeg?auto=compress&cs=tinysrgb&h=226.525&fit=crop&w=253.17499999999998&dpr=2" alt="" />
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/8438874/pexels-photo-8438874.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     {/* <div>
                     <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
@@ -20,10 +20,10 @@ function Gallery() {
                 </div>
                 <div class="grid gap-4">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/7869036/pexels-photo-7869036.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/8566569/pexels-photo-8566569.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     {/* <div>
                     <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
@@ -31,10 +31,10 @@ function Gallery() {
                 </div>
                 <div class="grid gap-4">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/7868822/pexels-photo-7868822.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/8294682/pexels-photo-8294682.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     {/* <div>
                     <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
@@ -42,10 +42,10 @@ function Gallery() {
                 </div>
                 <div class="grid gap-4">
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/9243229/pexels-photo-9243229.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     <div>
-                        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+                        <img class="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/7869090/pexels-photo-7869090.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
                     </div>
                     {/* <div>
                     <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
