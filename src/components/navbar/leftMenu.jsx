863d176debc28f8aa9c4c8c284fa51dc492702d9
skillg2021 <atul02dubey07@gmail.com>
2025-01-09 05:57:28 +0530

    Update leftMenu.jsx

--- src/components/navbar/leftMenu.jsx
+++ src/components/navbar/leftMenu.jsx
@@ -4,9 +4,9 @@ import { Menu } from "antd";
 const LeftMenu = ({ mode }) => {
   return (
     <Menu mode={mode} className="bg-[#001650]">
-      <Menu.Item key="explore">Egdd</Menu.Item>
-      <Menu.Item key="features">Features</Menu.Item>
-      <Menu.Item key="about">About Us</Menu.Item>
+      <Menu.Item key="explore">Home</Menu.Item>
+      <Menu.Item key="features">Courses</Menu.Item>
+      <Menu.Item key="about">Our Services</Menu.Item>
       <Menu.Item key="contact">Contact Us</Menu.Item>
     </Menu>
   );
